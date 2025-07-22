4334feed6c46c983f0f4d19b293e93236a7f9fbe
1e1e1 <117133167+1e1e1@users.noreply.github.com>
2025-07-22 09:47:16 +0800

    遞迴版本2.cpp

---
+++ 遞迴版本2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void powerSet(string str, int index = 0, string curr = "") {
+    int n = str.length();
+
+    if (index == n) {
+        cout << "(";
+        for (size_t i = 0; i < curr.length(); ++i) {
+            cout << curr[i];
+            if (i != curr.length() - 1) cout << ",";
+        }
+        cout << ")" << endl;
+        return;
+    }
+
+    powerSet(str, index + 1, curr + str[index]); 
+    powerSet(str, index + 1, curr);               
+}
+
+int main() {
+    string str = "abc";
+    powerSet(str);
+    return 0;
+}
